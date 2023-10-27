0a0a76dc3f281f63ba6d1a5e82ad596cf796c90b
Darnel KOUA <darnelkoua1@gmail.com>
2023-10-27 12:54:51 +0000

    mise à jour churn

--- churn.R
+++ churn.R
@@ -1,1 +1,12 @@
+# Package
+library(readr)
+library(tidyverse)
 
+# Load data
+
+df <- read_delim("Data/DataSet_TDD.csv", 
+                          delim = ";", escape_double = FALSE, trim_ws = TRUE)
+df <- df %>% select(-ID) # delete ID
+head(df)
+dim(df)
+summary(df)
