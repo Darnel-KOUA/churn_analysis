3c8d9adafeec2c0938f5546db884363504189147
Darnel KOUA <darnelkoua1@gmail.com>
2023-10-27 22:21:16 +0000

    mise à jour

--- churn.R
+++ churn.R
@@ -7,6 +7,22 @@ library(tidyverse)
 df <- read_delim("Data/DataSet_TDD.csv", 
                           delim = ";", escape_double = FALSE, trim_ws = TRUE)
 df <- df %>% select(-ID) # delete ID
+
+# first six observations
 head(df)
+
+# last six observations
+tail(df)
+
+# Number of observations and variables
 dim(df)
+
+# Summary of data
 summary(df)
+
+# See the type of variables
+map_vec(df, typeof)  #no type complies for some variables, need correction for this.
+
+# percentage of missing values for each variable
+map_vec(df, \(x) round((sum(is.na(x))/dim(df)[1])*100, 2))
+
