5da5745bef6293ad6c26490ad24962277f5a0d70
Darnel KOUA <darnelkoua1@gmail.com>
2023-10-28 00:02:39 +0000

    mise à jour

--- churn.R
+++ churn.R
@@ -26,3 +26,18 @@ map_vec(df, typeof)  #no type complies for some variables, need correction for t
 # percentage of missing values for each variable
 map_vec(df, \(x) round((sum(is.na(x))/dim(df)[1])*100, 2))
 
+# transform categorical variables into factor
+name_fact <- names(df[, 1:10])
+
+df <- df %>% modify_if(names(df) %in% name_fact, as.factor)
+
+#Or
+#df <- df %>% modify_at(vars(CHURN:STATUT_FACT_M2), as.factor)
+
+# Barplot for CHURN target
+barplot(table(df$CHURN), xlab = "CHURN", ylab = "Effectif", 
+        legend.text = c("No Churn", "Churn"), col = c("blue", "red"), 
+        args.legend = list(x = "topright"))
+# we can see the imbalance between classes
+
+#treatment of outliers
